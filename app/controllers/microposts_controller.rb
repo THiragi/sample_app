1c552df04ba4b53bc79f05c2e6862dbe5f9d128c
EC2 Default User <ec2-user@ip-172-31-44-252.ap-southeast-1.compute.internal>
2018-07-20 09:43:40 +0000

    Finish Image Uploader

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,7 +20,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:image, :content)
     end
 
     def correct_user
