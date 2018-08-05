295a545fa1bdf15ee96499632da4c6805b9d29ac
EC2 Default User <ec2-user@ip-172-31-44-252.ap-southeast-1.compute.internal>
2018-08-05 05:35:57 +0000

    Fix Contact Form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,9 +7,12 @@ class ContactMailer < ActionMailer::Base
   #   en.contact_mailer.sent.subject
   #
   def sent(contact)
-
     @contact = contact
-
     mail(to: @contact.email, subject: "Thank you for your contact!")
+  end
+  
+  def receive(contact)
+    @contact = contact
+    mail(to: "w1th1nmybr41n@gmail.com", subject: "Inquery from your customer!")
   end
 end
