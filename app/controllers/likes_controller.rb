efabea5ee873438fbe60fc623e02cdc1fca3412b
EC2 Default User <ec2-user@ip-172-31-44-252.ap-southeast-1.compute.internal>
2018-08-26 00:41:08 +0000

    Finish Ajax Follow-Button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,12 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
   #現在のUserのuser_idで指定のmicropostにLikeを作る。
   def create
-    #micropost = Micropost.find params[:micropost_id]
-    #micropost.likes.create(user_id: current_user.id)
-    #redirect_to micropost.user
+    micropost = Micropost.find params[:micropost_id]
+    micropost.likes.create(user_id: current_user.id)
+    redirect_to micropost.user
+
     
-    like = Like.create(user_id: current_user.id, micropost_id: params[:micropost_id])
+    #like = Like.create(user_id: current_user.id, micropost_id: params[:micropost_id])
     #user = like.micropost.user
     #redirect_to user
 
@@ -14,13 +15,13 @@ class LikesController < ApplicationController
 
   #指定のMicropostに現在のUserのuser_idで作られたLikeを探し、削除する。
   def destroy
-    #micropost = Micropost.find params[:micropost_id]
-    #micropost.likes.where(user_id: current_user.id).destroy_all
-    #redirect_to micropost.user
+    micropost = Micropost.find params[:micropost_id]
+    micropost.likes.where(user_id: current_user.id).destroy_all
+    redirect_to micropost.user
     
-    like = Like.find_by(user_id: current_user.id, micropost_id: params[:micropost_id])
+    #like = Like.find_by(user_id: current_user.id, micropost_id: params[:micropost_id])
     #user = like.micropost.user
-    like.destroy
+    #like.destroy
     #redirect_to user
 
   end
